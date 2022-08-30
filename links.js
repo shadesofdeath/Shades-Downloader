507c2d2cd9543438685d518f1ad380412d788666
Berkay AY <61064047+berkays0733@users.noreply.github.com>
2022-08-30 06:23:50 -0700

    Big Update

--- links.js
+++ links.js
@@ -2,16 +2,296 @@ var select = document.getElementById('site');
 
 document.getElementById("button").onclick = function(){
     var value = select.value
-    if (value == "win11")
+    if (value == "11-22h264")
     {
         window.location.href = "https://archive.org/download/22622.575.220729-1215.-ni-release-svc-prod-2-clientmulti-x-64-fre-en-us/22621.575.220729-1215.NI_RELEASE_SVC_PROD2_CLIENTMULTI_X64FRE_EN-US.ISO";
     }
-    else if (value == "win10")
+    else if (value == "11-21h264")
     {
-        window.location.href = "https://www.yahoo.com/";
+        window.location.href = "https://archive.org/download/windows-11-21-h-2-notpm/Windows%2011%20%2821H2%29%20NOTPM.iso";
     }
-    else if (value == "win8")
+    else if (value == "10-22h264")
     {
-        window.location.href = "https://www.msn.com/";
+        window.location.href = "https://archive.org/download/windows-10-22-h-2-19045.1806/WINDOWS%2010%2022H2%2019045.1806.iso";
+    }
+    else if (value == "10-22h232")
+    {
+        window.location.href = "https://archive.org/download/windows-10-insider-preview-19045.1826/Windows10_InsiderPreview_Client_x32_en-us_19045.1826.iso";
+    }
+    else if (value == "10-21h264")
+    {
+        window.location.href = "https://archive.org/download/win-10-21-h-2-english-x-64_20220529/Win10_21H2_English_x64.iso";
+    }
+    else if (value == "10-21h232")
+    {
+        window.location.href = "https://archive.org/download/win-10-21-h-2-english-x-32_202205/Win10_21H2_English_x32.iso";
+    }       
+    else if (value == "20h264")
+    {
+        window.location.href = "https://archive.org/download/win-10-20-h-2-english-x-64_202011/Win10_20H2_English_x64.iso";
+    }    
+    else if (value == "20h232")
+    {
+        window.location.href = "https://archive.org/download/win-10-20-h-2-english-x-32/Win10_20H2_English_x32.iso";
+    }    
+    else if (value == "200464")
+    {
+        window.location.href = "https://archive.org/download/win-10-2004-english-x-64_202009/Win10_2004_English_x64.iso";
+    }    
+    else if (value == "200432")
+    {
+        window.location.href = "https://archive.org/download/win-10-2004-english-x-32/Win10_2004_English_x32.iso";
+    }    
+    else if (value == "190964")
+    {
+        window.location.href = "https://archive.org/download/win101909englishx64_20200111/Win10_1909_English_x64.iso";
+    }    
+    else if (value == "190932")
+    {
+        window.location.href = "https://archive.org/download/win101909englishx64_20200111/Win10_1909_English_x64.iso";
+    }    
+    else if (value == "190364")
+    {
+        window.location.href = "https://archive.org/download/Windows101903V1EnglishX32/Windows_10_1903_V1_English_x64.iso";
+    }    
+    else if (value == "190332")
+    {
+        window.location.href = "https://archive.org/download/Windows101903V1EnglishX32/Windows_10_1903_V1_English_x32.iso";
+    }    
+    else if (value == "180964")
+    {
+        window.location.href = "https://archive.org/download/win-10-1809-oct-v-2-english-international-x64/Win10_1809Oct_v2_EnglishInternational_x64.iso";
+    }    
+    else if (value == "180932")
+    {
+        window.location.href = "https://archive.org/download/win101809/%28GetMyOS%29Windows_10_Multi%28Home_Pro%29_X86_V1809_Nov_EN-GB.iso";
+    }    
+    else if (value == "180364")
+    {
+        window.location.href = "https://archive.org/download/win101803englishx64/Win10_1803_English_x64.iso";
+    }    
+    else if (value == "180332")
+    {
+        window.location.href = "https://archive.org/download/sw-dvd-5-win-pro-ent-edu-n-10-1803-32-bit-english/SW_DVD5_Win_Pro_Ent_Edu_N_10_1803_32BIT_English_-2_MLF_X21-79646.iso";
+    }    
+    else if (value == "170964")
+    {
+        window.location.href = "https://archive.org/download/windows-10-1709/Windows%2010%201709.iso";
+    }    
+    else if (value == "170932")
+    {
+        window.location.href = "https://archive.org/download/windows-10-1709/Windows%2010%201709.iso";
+    }    
+    else if (value == "170364")
+    {
+        window.location.href = "https://archive.org/download/PCappsdad.comWindows10RS2V.170315063.540X64/_PCappsdad.com_Windows_10_RS2_V.1703_15063.540_X64.iso";
+    }    
+    else if (value == "170332")
+    {
+        window.location.href = "https://archive.org/download/PCappsdad.comWindows10RS2V.170315063.540X64/_PCappsdad.com_Windows_10_RS2_V.1703_15063.540X86_Aio.iso";
+    }    
+    else if (value == "160764")
+    {
+        window.location.href = "https://archive.org/download/microsoft-windows-10-1607-oct-2016-64-bit/Microsoft_Windows_10_1607_Oct_2016_64_Bit.iso";
+    }    
+    else if (value == "160732")
+    {
+        window.location.href = "https://archive.org/download/win-10-1607-english-home-pro-x-32/Win10_1607_English%20HOME-PRO_x32.iso";
+    }    
+    else if (value == "151164")
+    {
+        window.location.href = "https://archive.org/download/win10-1511/Win10_1511_EnglishInternational_x64.iso";
+    }    
+    else if (value == "151132")
+    {
+        window.location.href = "https://archive.org/download/win10-1511/Win10_1511_EnglishInternational_x32.iso";
+    }    
+    else if (value == "150764")
+    {
+        window.location.href = "https://archive.org/download/win-10-1507-english-x-64/Win10_1507_English_x64.iso";
+    }    
+    else if (value == "150732")
+    {
+        window.location.href = "https://archive.org/download/windows-10-home-pro-1507-x-86/Windows%2010%20Home%2C%20Pro%201507%20x86.iso";
+    }    
+    else if (value == "10ltsc64")
+    {
+        window.location.href = "https://archive.org/download/en_windows_10_enterprise_ltsc_2019/en_windows_10_enterprise_ltsc_2019_x64_dvd_5795bb03.iso";
+    }    
+    else if (value == "10ltsc32")
+    {
+        window.location.href = "https://archive.org/download/en_windows_10_enterprise_ltsc_2019/en_windows_10_enterprise_ltsc_2019_x86_dvd_892869c9.iso";
+    }    
+    else if (value == "home64")
+    {
+        window.location.href = "https://archive.org/download/win-8.1/Win8.1_EnglishInternational_x64.iso";
+    }    
+    else if (value == "home32")
+    {
+        window.location.href = "https://archive.org/download/win-8.1/Win8.1_EnglishInternational_x32.iso";
+    }    
+    else if (value == "pro64")
+    {
+        window.location.href = "https://archive.org/download/en_windows_8_1_pro_vl_x64_dvd_2971948_202107/en_windows_8_1_pro_vl_x64_dvd_2971948.iso";
+    }    
+    else if (value == "pro32")
+    {
+        window.location.href = "https://archive.org/download/en_windows_8_1_pro_vl_x86_dvd_2972633_202107/en_windows_8_1_pro_vl_x86_dvd_2972633.iso";
+    }    
+    else if (value == "enterprise64")
+    {
+        window.location.href = "https://archive.org/download/en_windows_8_1_enterprise_x64_dvd_2971902_202107/en_windows_8_1_enterprise_x64_dvd_2971902.iso";
+    }    
+    else if (value == "enterprise32")
+    {
+        window.location.href = "https://archive.org/download/en_windows_8_1_enterprise_x86_dvd_2972289_202107/en_windows_8_1_enterprise_x86_dvd_2972289.iso";
+    }    
+    else if (value == "starter64")
+    {
+        window.location.href = "https://archive.org/download/windows-7-starter-x-64-sp-1/Windows%207%20Starter%20x64%20SP1.iso";
+    }    
+    else if (value == "starter32")
+    {
+        window.location.href = "https://archive.org/download/en_windows_7_starter_x86_dvd_x15-68956_202107/en_windows_7_starter_x86_dvd_x15-68956.iso";
+    }    
+    else if (value == "basic64")
+    {
+        window.location.href = "https://archive.org/download/Win7HomeBasicSP1x64/Windows%207%20Home%20Basic%20SP1%20%2864%20Bit%29.iso";
+    }    
+    else if (value == "basic32")
+    {
+        window.location.href = "https://archive.org/download/en_windows_7_home_basic_x86_dvd_x15-65654_202107/en_windows_7_home_basic_x86_dvd_x15-65654.iso";
+    }    
+    else if (value == "premium64")
+    {
+        window.location.href = "https://archive.org/download/windows-7-home-premium-x-64-sp-1/Windows-7-Home-Premium-x64-SP1.iso";
+    }    
+    else if (value == "premium32")
+    {
+        window.location.href = "https://archive.org/download/windows-7-home-premium-x-64-sp-1/Windows-7-Home-Premium-x86-SP1.iso";
+    }    
+    else if (value == "ulti64")
+    {
+        window.location.href = "https://archive.org/download/en_windows_7_ultimate_x64_dvd_x15-65922_202107/en_windows_7_ultimate_x64_dvd_x15-65922.iso";
+    }    
+    else if (value == "ulti32")
+    {
+        window.location.href = "https://archive.org/download/en_windows_7_ultimate_x86_dvd_x15-65921_202107/en_windows_7_ultimate_x86_dvd_x15-65921.iso";
+    }    
+    else if (value == "prof64")
+    {
+        window.location.href = "https://archive.org/download/microsoft-windows-7-professional-x-64-en-us/Microsoft%20Windows%207%20PROFESSIONAL%20x64%20en-us.iso";
+    }    
+    else if (value == "prof32")
+    {
+        window.location.href = "https://archive.org/download/microsoft-windows-7-professional-x-64-en-us/Microsoft%20Windows%207%20PROFESSIONAL%20x86%20en-us.iso";
+    }    
+    else if (value == "7enterprise64")
+    {
+        window.location.href = "https://archive.org/download/en_windows_7_enterprise_x64_dvd_x15-70749_202107/en_windows_7_enterprise_x64_dvd_x15-70749.iso";
+    }    
+    else if (value == "7enterprise32")
+    {
+        window.location.href = "https://archive.org/download/en_windows_7_enterprise_x86_dvd_x15-70745_202107/en_windows_7_enterprise_x86_dvd_x15-70745.iso";
+    }    
+    else if (value == "activator")
+    {
+        window.location.href = "https://github.com/massgravel/Microsoft-Activation-Scripts/releases/download/1.6/MAS_1.6_Password_1234.7z";
+    }
+    else if (value == "ubuntu")
+    {
+        window.location.href = "https://ftp.ulak.net.tr/ubuntu-releases/22.04.1/ubuntu-22.04.1-desktop-amd64.iso";
+    }
+    else if (value == "mint")
+    {
+        window.location.href = "https://mirrors.edge.kernel.org/linuxmint/stable/21/linuxmint-21-cinnamon-64bit.iso";
+    }
+    else if (value == "pop")
+    {
+        window.location.href = "https://iso.pop-os.org/22.04/amd64/intel/12/pop-os_22.04_amd64_intel_12.iso";
+    }
+    else if (value == "zorin")
+    {
+        window.location.href = "https://mirror.ihost.md/zorinos/16/Zorin-OS-16.1-Core-64-bit.iso";
+    }
+    else if (value == "elementary")
+    {
+        window.location.href = "https://sgp1.dl.elementary.io/download/MTY2MTcxMzk5MQ==/elementaryos-6.1-stable.20211218-rc.iso";
+    }
+    else if (value == "cent")
+    {
+        window.location.href = "http://mirror.alastyr.com/centos/7.9.2009/isos/x86_64/CentOS-7-x86_64-DVD-2009.iso";
+    }
+    else if (value == "solus")
+    {
+        window.location.href = "https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-Budgie.iso";
+    }
+    else if (value == "2007")
+    {
+        window.location.href = "https://archive.org/download/microsoft-office-2007-preactivated/Microsoft%20Office%202007.iso";
+    }
+    else if (value == "2010")
+    {
+        window.location.href = "https://archive.org/download/microsoft-office-2010-professional-english_202005/Microsoft%20Office%202010%20Professional%20English.zip";
+    }
+    else if (value == "2013")
+    {
+        window.location.href = "https://archive.org/download/office-2013-64bit/Office_2013_32bit.iso";
+    }
+    else if (value == "2016")
+    {
+        window.location.href = "https://archive.org/download/office-2016_202110/Office%202016.iso";
+    }
+    else if (value == "2019")
+    {
+        window.location.href = "https://archive.org/download/microsoft-office-2019_202110/Microsoft%20Office%202019.iso";
+    }
+    else if (value == "2021")
+    {
+        window.location.href = "https://archive.org/download/microsoft-office-2021-pro-plus-v-2103-build-13901.20400-x-64-file-cr/Microsoft%20Office%202021Pro%20Plus%20v2103%20Build%2013901.20400%20x64%20%5BFileCR%5D.zip";
+    }
+    else if (value == "libre")
+    {
+        window.location.href = "https://ftp.gwdg.de/pub/tdf/libreoffice/stable/7.4.0/win/x86/LibreOffice_7.4.0_Win_x86.msi";
+    }
+    else if (value == "activator")
+    {
+        window.location.href = "https://github.com/massgravel/Microsoft-Activation-Scripts/releases/download/1.6/MAS_1.6_Password_1234.7z";
+    }
+    else if (value == "photoshop")
+    {
+        window.location.href = "https://archive.org/download/adobe-photoshop-2022-23.2.2/Adobe_Photoshop_2022_23.2.2.iso";
+    }
+    else if (value == "premier")
+    {
+        window.location.href = "https://archive.org/download/setup_20220124/Setup.exe";
+    }
+    else if (value == "ıllustrator")
+    {
+        window.location.href = "https://archive.org/download/adobe-illustrator-2022-26.1/Adobe_Illustrator_2022_26.1.iso";
+    }
+    else if (value == "acrobat")
+    {
+        window.location.href = "https://archive.org/download/adobe.-acrobat.-pro.-dc.-2022.x-64.-multilingual/Adobe.Acrobat.Pro.DC.2022.x64.Multilingual.iso";
+    }
+    else if (value == "chrome")
+    {
+        window.location.href = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B56D0894A-9830-6B18-ECB9-D28E4FE6CFAD%7D%26lang%3Dtr%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe";
+    }
+    else if (value == "operagx")
+    {
+        window.location.href = "https://net.geo.opera.com/opera_gx/stable/windows?utm_tryagain=yes&utm_source=opera.com_via_opera_com&utm_medium=roc&utm_campaign=(none)_via_opera_com_https&utm_content=%2Ftr%2Fgx&http_referrer=https%3A%2F%2Fwww.opera.com%2Ftr%2Fgx%3Fedition%3Dstd-1%26utm_content%3D3323_DL%26utm_source%3DPWNgames%26utm_medium%3Dpa%26utm_campaign%3DPWN_TR%26utm_id%3D96e5d5cffc10432f9ba16ae932a7a03a&utm_site=opera_com&&utm_lastpage=opera.com/gx&dl_token=70827192";
+    }
+    else if (value == "malware")
+    {
+        window.location.href = "https://www.malwarebytes.com/api/downloads/mb-windows?filename=MBSetup-37335.37335.exe";
+    }
+    else if (value == "java")
+    {
+        window.location.href = "https://sdlc-esd.oracle.com/ESD6/JSCDL/jdk/8u341-b10/424b9da4b48848379167015dcc250d8d/jre-8u341-windows-x64.exe?GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/8u341-b10/424b9da4b48848379167015dcc250d8d/jre-8u341-windows-x64.exe&BHost=javadl.sun.com&File=jre-8u341-windows-x64.exe&AuthParam=1661892129_5654c34df983685258772839f16e7b90&ext=.exe";
+    }
+    else if (value == "7zip")
+    {
+        window.location.href = "https://d3.7-zip.org/a/7z2201-x64.exe";
     }
 };
